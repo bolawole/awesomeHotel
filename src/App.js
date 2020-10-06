b47f1be2966c6c89611fe6317c522b8d7a363fc5
Adewole Awe <dewoleawe@gmail.com>
2020-10-06 15:34:00 -0400

    new commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function App() {
-	return <div className="App">AWesome Lets go 🚀 </div>;
+	return <div className="App">AWesome Lets go 🚀 hello world</div>;
 }
 
 export default App;
